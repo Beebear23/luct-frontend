1605f7b8d48cbb7860396303284aa07ebfd81230
Beebear23 <mamoshanyanab@gmail.com>
2025-10-06 07:31:17 +0200

    Updated frontend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE = process.env.REACT_APP_API_URL || 'https://luct-backend.onrender.com';
 
 export default API_BASE;
